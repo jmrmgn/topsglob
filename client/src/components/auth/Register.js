3dd50b90a1877cdca60266fb897aceb33ef600d9
Jomar <jomar.magno026@gmail.com>
2019-01-14 19:31:42 +0800

    user registration form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,59 @@
 import React, { Component } from 'react';
 
 class Register extends Component {
+   onSubmit = e => {
+      e.preventDefault();
+   }
+
    render() {
       return (
-         <div>
-            <h1 className="title">Register Page...</h1>
-         </div>
+         <div className="block">
+            <div className="columns">
+               <div className="column is-8 is-offset-2">
+                  <h1 className="title has-text-centered">Register new user</h1>
+                  <div className="columns">
+                     <div className="column is-6 is-offset-3">
+                        <form onSubmit={this.onSubmit.bind(this)}>
+                           <div className="field">
+                              <label htmlFor="username">Username</label>
+                              <div className="control">
+                                 <input type="text" className="input" placeholder="Enter username" />
+                              </div>
+                              <p className="help">This is a help text</p>
+                           </div>
+                           
+                           <div className="field">
+                              <label htmlFor="email">Email</label>
+                              <div className="control">
+                                 <input type="email" className="input" placeholder="Enter email" />
+                              </div>
+                              <p className="help">This is a help text</p>
+                           </div>
+                           <div className="field">
+                              <label htmlFor="password">Password</label>
+                              <div className="control">
+                                 <input type="password" className="input" placeholder="Enter password" />
+                              </div>
+                              <p className="help">This is a help text</p>
+                           </div>
+                           <div className="field">
+                              <label htmlFor="confirmPassword">Confirm Password</label>
+                              <div className="control">
+                                 <input type="password" className="input" placeholder="Confirm password" />
+                              </div>
+                              <p className="help">This is a help text</p>
+                           </div>
+                           <div className="field" style={{ marginTop: 30 }}>
+                              <div className="control">
+                                 <button className="button is-primary is-fullwidth">Register</button>
+                              </div>
+                           </div>
+                        </form>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            </div>
       )
    }
 }
