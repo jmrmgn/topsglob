8f768cbb6cc51b611cf3660488da8127543bb8ca
Jomar <jomar.magno026@gmail.com>
2019-01-14 18:59:48 +0800

    public components and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'bulma/css/bulma.min.css';
 import './App.css';
+
+// Components
+import Navbar from './components/layout/Navbar';
+import Home from './components/layout/Home';
+import Posts from './components/posts/Posts';
+import Users from './components/users/Users';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
 
 class App extends Component {
    render() {
       return (
-         <div className="App">
-
-         </div>
+         <Router>
+            <div className="App">
+               <Navbar />
+               <div className="container">
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/posts" component={Posts} />
+                  <Route exact path="/users" component={Users} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
+               </div>
+            </div>
+         </Router>
       );
    }
 }
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import classnames from 'classnames';
+import { NavLink } from 'react-router-dom';
+
+class Navbar extends Component {
+   state = {
+      isActive: false
+   }
+
+   onClick = e => {
+      e.preventDefault();
+      this.setState({ isActive: !this.state.isActive });
+   }
+
+   render() {
+      const { isActive } = this.state;
+      return (
+         <div className="block">
+            <nav className="navbar is-primary is-spaced has-shadow" role="navigation" aria-label="main navigation">
+               <div className="container">
+                  <div className="navbar-brand">
+                     <NavLink className="navbar-item" to="/">
+                        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" alt="" />
+                     </NavLink>
+                     <a 
+                        href="/"
+                        onClick={this.onClick.bind(this)}
+                        aria-label="menu"
+                        aria-expanded="false"
+                        data-target="main-navbar"
+                        className={classnames('navbar-burger burger', { 'is-active': isActive })}
+                     >
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                     </a>
+                  </div>
+                  
+                  <div
+                     id="main-navbar"
+                     className={classnames('navbar-menu', { 'is-active': isActive })}
+                  >
+                     <div className="navbar-start">
+                        <NavLink className="navbar-item" to="/">Home</NavLink>
+                        <NavLink className="navbar-item" to="/posts">Posts</NavLink>
+                        <NavLink className="navbar-item" to="/users">Users</NavLink>
+                     </div>
+                     <div className="navbar-end">
+                        <NavLink className="navbar-item" to="/register">Register</NavLink>
+                        <NavLink className="navbar-item" to="/login">Login</NavLink>
+                     </div>
+                  </div>
+               </div>
+            </nav>
+         </div>
+      )
+   }
+}
+
+export default Navbar;
